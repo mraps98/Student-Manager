cf2d6e31463b5476c9a716772148ada4a4d2ae05
Preet <amritpreet98@gmail.com>
2019-02-12 02:29:52 +0530

    UseUrls:

---
+++ Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_Manager.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public DateTime Dob { get; set; }
+        public string Nationality { get; set; }
+        public string Major { get; set; }
+    }
+}
